2144085dff17eefe8ea8ac97d8042bd553b195ea
PH1L-7 <lussier.philippe@gmail.com>
2023-05-03 15:47:53 -0400

    commit tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres tres important

---
+++ Exercice1+2Tests/Controllers/HomeControllerShould.cs
@@ -0,0 +1,30 @@
+﻿using Exercice2.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1_2Tests.Controllers
+{
+    public class HomeControllerShould
+    {
+
+        [Fact]
+        public void Home()
+        {
+            var homeCC = new HomeController();
+            var result = homeCC.Index3();
+            var TextResult = Assert.IsType<ViewResult>(result);
+        }
+        /*
+        2.1 GOOD
+        2.2 GOOD
+        2.3 GOOD
+        2.4 N//A
+        2.5 N//A
+        2.6 N//A
+        */
+    }
+}
